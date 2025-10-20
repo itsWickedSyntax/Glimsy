d8c2f78c702e0d3bf4f68348b337a9a7e7f4715c
wickedSyntax <inkdass@hotmail.com>
2025-10-20 20:19:43 +0300

    Delete Glimsy directory

--- Glimsy/glimsy-ecommerce/src/utils/currency.ts
+++
@@ -1,6 +0,0 @@
-export const formatCurrency = (amount: number, currency: string = 'KWD'): string => {
-    return new Intl.NumberFormat('en-KW', {
-        style: 'currency',
-        currency: currency,
-    }).format(amount);
-};
